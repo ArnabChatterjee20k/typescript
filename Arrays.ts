d825569b21b543e55fe871e6854546e9f59405dc
arnab <arnabchatterjee.ac.2@gmail.com>
2023-07-04 22:03:47 +0530

    lesson - arrays and literal types

---
+++ Arrays.ts
@@ -0,0 +1,16 @@
+const numberArr: number[] = [1, 2, 3.12, 12.0, 4, 0.1]; // all must be numbers
+
+// either all of them should be numbers of strings
+let string_or_number_Arr: string[] | number[] = [1, 2, 3];
+string_or_number_Arr = ["ar", "df", "dfd"];
+
+// combination of number and string
+let string_and_number_and_bool_Arr: (string | number | boolean)[] = [
+  1,
+  "dsf",
+  "fd",
+  12,
+  1.01,
+  true,
+  false,
+];
---
+++ LiteralType.ts
@@ -0,0 +1,2 @@
+// seat will be either any of the three options not other will be allowed
+let seat:"aisle"|"middle"|"window"
