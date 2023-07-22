92db256cd5356ba8dc561e32944140c3796cc64e
arnab <arnabchatterjee.ac.2@gmail.com>
2023-07-22 22:37:44 +0530

    lesson - dom manipulation

---
+++ DomManipulation/src/main.ts
@@ -0,0 +1,9 @@
+// const app = document.getElementById("app")
+// const p = document.createElement("p")
+
+// p.textContent = "Helllo, world"
+// app?.appendChild(p)
+
+const div = document.querySelector<HTMLDivElement>("div")
+console.log(div?.children);
+console.log(div?.childNodes);
